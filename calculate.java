42c372a7a00f060a1048fd61bf484e12eb6ac205
Ashajessica16-asha <vinojessica16@gmail.com>
2026-07-21 15:24:44 +0530

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,19 @@
+public class calculate{
+int add(int a,int b) {
+return a+b;
+}
+double add(double a,double b){
+return a+b;
+}
+int add(int a,int b,int c){
+return a+b+c;
+}
+public static void main(String[]args){
+calculate obj=new calculate();
+System.out.println("Sum of two integers(5,10):"+obj.add(5,10));
+System.out.println("Sum of two doubles(5.5,2.3):"+obj.add(5.5,2.3));
+System.out.println("Sum of three integers(2,4,6):"+obj.add(2,4,6));
+}
+}
+
+
