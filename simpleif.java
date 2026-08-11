ce62d3fe59c377bbdc680834129c7ac02fc57ee8
Ashajessica16-asha <vinojessica16@gmail.com>
2026-08-11 15:40:55 +0530

    Add files via upload

---
+++ simpleif.java
@@ -0,0 +1,10 @@
+public class simpleif {
+  public static void main (String[]args) {
+  byte age=17;
+  if(age>=18)
+  {
+   System.out.println("Eligible to vote");
+  }
+   System.out.println("End if");
+  }
+}
